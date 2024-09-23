ea66279f3e3f066cbbf3e557c64b4527272f4415
stas_fr <stas240600@yandex.ru>
2024-09-23 22:33:57 +0300

    chore: update ts and vite configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,64 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import VueMacros from "unplugin-vue-macros/vite";
+import { VueRouterAutoImports } from "unplugin-vue-router";
+import VueRouter from "unplugin-vue-router/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    // https://github.com/posva/unplugin-vue-router
+    VueRouter({
+      routesFolder: [
+        {
+          src: "src/views",
+          path: "",
+        },
+      ],
+    }),
     vue(),
     vueDevTools(),
+    VueMacros({
+      defineOptions: false,
+      defineModels: false,
+      plugins: {
+        vue: vue({
+          script: {
+            propsDestructure: true,
+            defineModel: true,
+          },
+        }),
+      },
+    }),
+
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        "vue",
+        "@vueuse/core",
+        VueRouterAutoImports,
+        {
+          // add any other imports you were relying on
+          "vue-router/auto": ["useLink"],
+        },
+      ],
+      dts: true,
+      vueTemplate: true,
+    }),
+    // https://github.com/antfu/vite-plugin-components
+    Components({
+      dts: true,
+      dirs: ["./src/components"],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
